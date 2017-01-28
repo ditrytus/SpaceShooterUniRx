3b77fed592ba6b3db1ddc16efc1fa4d58abc73e5
Jakub Gruszecki <jakubgruszecki@MacBook-Pro-Admin-2.local>
2017-01-28 22:45:46 +0100

    Shooting an asteroid.

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class DestroyOnCollision : MonoBehaviour {
+	void Start () {
+		this.OnTriggerEnterAsObservable()
+			.Where(c => c.gameObject.tag != "boundary")
+			.Subscribe(c => {
+				Destroy(c.gameObject);
+				Destroy(gameObject);
+			});
+	}
+}
---
+++ Assets/Scripts/RotateRandomly.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRandomly : MonoBehaviour {
+
+	public float tumble;
+
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
