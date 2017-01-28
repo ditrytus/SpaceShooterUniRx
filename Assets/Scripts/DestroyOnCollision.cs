6a134272fd2059dc80600137c858ffcf1ba103f4
Jakub Gruszecki <jakubgruszecki@MacBook-Pro-Admin-2.local>
2017-01-28 23:16:32 +0100

    Adding explosions to the collision.

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
 public class DestroyOnCollision : MonoBehaviour {
+
+	public GameObject asteroidExplostion;
+
+	public GameObject playerExplostion;
+
 	void Start () {
-		this.OnTriggerEnterAsObservable()
-			.Where(c => c.gameObject.tag != "boundary")
+		var collisions = this.OnTriggerEnterAsObservable()
+			.Where(c => c.gameObject.tag != "boundary");
+		
+		collisions.Subscribe(c => {
+			Destroy(c.gameObject);
+			Destroy(gameObject);
+			Instantiate(asteroidExplostion, transform.position, transform.rotation);
+		});
+
+		collisions
+			.Where(c => c.gameObject.tag == "Player")
 			.Subscribe(c => {
-				Destroy(c.gameObject);
-				Destroy(gameObject);
+				Instantiate(playerExplostion, c.transform.position, c.transform.rotation);
 			});
 	}
 }
